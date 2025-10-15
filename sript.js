0d8d3c49286fe5aa6b1421fc4d99081db702efc5
Iara Carballo <iaracarballo09@gmail.com>
2025-10-14 22:43:22 -0300

    Primer super sayajin repo

---
+++ sript.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const boton = document.getElementById("boton");
+  boton.addEventListener("click", function() {
+    alert("¡Hola! Soy Goku 🐉");
+  });
+});
